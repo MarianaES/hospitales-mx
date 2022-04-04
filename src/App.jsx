8c40ba3b9083ae5438012ba89cc79cb0a7957d68
MarianaES <mariana.enriquez.s@gmail.com>
2022-04-03 20:31:49 -0500

    Implement NavBar and SearchParams

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,45 @@
 import { render } from "react-dom";
-import Hospital from "./Hospital";
-import { Box, Container, CssBaseline } from "@mui/material";
-import SearchAppBar from "./components/SearchBar";
+import { CssBaseline, Grid } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
+
+import Hospital from "./Hospital";
+import NavBar from "./components/NavBar";
+import SearchParams from "./components/SearchParams";
 import theme from "./GlobalStyles";
 
 function App() {
   return (
     <CssBaseline>
       <ThemeProvider theme={theme}>
-        <SearchAppBar />
-        <Container>
-          <Hospital
-            name="DIF AGUASCALIENTES"
-            state="AGUASCALIENTES"
-            municipality="AGUASCALIENTES"
-          />
-          <Hospital
-            name="DIF COSÍO"
-            state="AGUASCALIENTES"
-            municipality="COSÍO"
-          />
-          <Hospital
-            name="DIF RINCÓN DE ROMOS"
-            state="AGUASCALIENTES"
-            municipality="RINCÓN DE ROMOS"
-          />
-        </Container>
+        <NavBar />
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          margin="4rem"
+          spacing={8}
+        >
+          <Grid item xs={4}>
+            <SearchParams />
+          </Grid>
+          <Grid item xs={8}>
+            <Hospital
+              name="DIF AGUASCALIENTES"
+              state="AGUASCALIENTES"
+              municipality="AGUASCALIENTES"
+            />
+            <Hospital
+              name="DIF COSÍO"
+              state="AGUASCALIENTES"
+              municipality="COSÍO"
+            />
+            <Hospital
+              name="DIF RINCÓN DE ROMOS"
+              state="AGUASCALIENTES"
+              municipality="RINCÓN DE ROMOS"
+            />
+          </Grid>
+        </Grid>
       </ThemeProvider>
     </CssBaseline>
   );
