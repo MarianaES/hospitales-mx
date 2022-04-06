f3f8937dd7dd3801263b5c658679fa461bc72cc9
MarianaES <mariana.enriquez.s@gmail.com>
2022-04-05 23:05:52 -0500

    Add light and dark colors

--- src/GlobalStyles.jsx
+++ src/GlobalStyles.jsx
@@ -1,56 +1,72 @@
-import { createTheme } from "@mui/material/styles";
+import { createTheme } from '@mui/material/styles'
 
 const Colors = {
-  primary: "#dc6f69",
-  secondary: "#777aa6",
-  success: "#4caf50",
-  info: "#2196f3",
-  warning: "#ff9800",
-  error: "#f44336",
+  primary: '#dc6f69',
+  primaryLight: '#e38b87',
+  primaryDark: '#9a4d49',
+  secondary: '#777aa6',
+  secondaryLight: '#9294b7',
+  secondaryDark: '#535574',
+  success: '#4caf50',
+  info: '#2196f3',
+  warning: '#ff9800',
+  error: '#f44336',
 
-  white: "#fff",
-  black: "#000",
-  gray: "#5d5c5c",
-};
+  white: '#fff',
+  black: '#000',
+  gray: '#5d5c5c',
+}
 
 const theme = createTheme({
   palette: {
-    type: "light",
+    type: 'light',
+    // background: {
+    //   default: Colors.primaryDark,
+    // },
     primary: {
       main: Colors.primary,
+      light: Colors.primaryLight,
+      dark: Colors.primaryDark,
     },
     secondary: {
       main: Colors.secondary,
+      light: Colors.secondaryLight,
+      dark: Colors.secondaryDark,
     },
     error: {
       main: Colors.error,
     },
+    text: {
+      white: Colors.white,
+      black: Colors.black,
+      gray: Colors.gray,
+    },
   },
   typography: {
-    fontFamily: "Lexend Deca",
+    fontFamily: 'Lexend Deca',
     fontSize: 14,
     body1: {
-      fontFamily: "Nunito",
+      fontFamily: 'Nunito',
     },
     body2: {
-      fontFamily: "Nunito",
+      fontFamily: 'Nunito',
     },
     subtitle1: {
-      fontFamily: "Saira",
+      fontFamily: 'Saira',
     },
     subtitle2: {
-      fontFamily: "Saira",
+      fontFamily: 'Saira',
     },
     button: {
-      fontFamily: "Lexend Deca",
+      fontFamily: 'Lexend Deca',
     },
     caption: {
-      fontFamily: "Lexend Deca",
+      fontFamily: 'Lexend Deca',
     },
     overline: {
-      fontFamily: "Lexend Deca",
+      fontFamily: 'Lexend Deca',
     },
   },
-});
+})
 
-export default theme;
+export default theme
