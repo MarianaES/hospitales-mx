e9197fbe5ba169f1c95029f8aebf7b45ce46ed61
MarianaES <mariana.enriquez.s@gmail.com>
2022-04-05 23:08:38 -0500

    Refactor SearchParams and Hospital

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,20 @@
-import { render } from "react-dom";
-import { CssBaseline, Grid } from "@mui/material";
-import { ThemeProvider } from "@mui/material/styles";
+import { render } from 'react-dom'
+import { CssBaseline } from '@mui/material'
+import { ThemeProvider } from '@mui/material/styles'
 
-import Hospital from "./components/Hospital";
-import NavBar from "./components/NavBar";
-import SearchParams from "./components/SearchParams";
-import theme from "./GlobalStyles";
+import NavBar from './components/NavBar'
+import SearchParams from './components/SearchParams'
+import theme from './GlobalStyles'
 
 function App() {
   return (
-    <CssBaseline>
-      <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
         <NavBar />
-        <Grid
-          container
-          direction="row"
-          justifyContent="center"
-          margin="4rem"
-          spacing={8}
-        >
-          <Grid item xs={4}>
-            <SearchParams />
-          </Grid>
-          <Grid item xs={8}>
-            <Hospital
-              name="DIF AGUASCALIENTES"
-              state="AGUASCALIENTES"
-              municipality="AGUASCALIENTES"
-            />
-            <Hospital
-              name="DIF COSÍO"
-              state="AGUASCALIENTES"
-              municipality="COSÍO"
-            />
-            <Hospital
-              name="DIF RINCÓN DE ROMOS"
-              state="AGUASCALIENTES"
-              municipality="RINCÓN DE ROMOS"
-            />
-          </Grid>
-        </Grid>
-      </ThemeProvider>
-    </CssBaseline>
-  );
+        <SearchParams />
+      </CssBaseline>
+    </ThemeProvider>
+  )
 }
 
-render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById('root'))
