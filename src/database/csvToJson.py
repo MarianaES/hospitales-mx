d6554f38b1adf5c22b5299f2e9636515ce1a1558
MarianaES <mariana.enriquez.s@gmail.com>
2022-04-12 22:03:36 -0500

    Delete moved files to client dir

--- src/database/csvToJson.py
+++
@@ -1,8 +0,0 @@
-# Run using iPython to convert CSV to JSON
-# Data source: Datos Abiertos (https://datos.gob.mx/busca/dataset/recursos-en-salud-nivel-central/resource/c1194dd2-5f55-441a-a1a1-2c4fe5ce971d)
-
-# import pandas as pd 
-
-# df = pd.read_csv("CLUES_2021(english).csv") 
-# with open('hospitals.json', 'w', encoding='utf-8') as file:
-#   df.to_json(file, orient='records', force_ascii=False)
