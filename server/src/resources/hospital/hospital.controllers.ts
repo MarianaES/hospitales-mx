a38b24138d85ce7cef045f4c0a6d4f83797ba88b
MarianaES <mariana.enriquez.s@gmail.com>
2022-04-21 22:18:57 -0500

    Include pagination to getAllHospitals controller

--- server/src/resources/hospital/hospital.controllers.ts
+++ server/src/resources/hospital/hospital.controllers.ts
@@ -7,6 +7,22 @@ import mongoose from 'mongoose'
 // @access Private
 
 export const getAllHospitals = asyncHandler(async (req, res) => {
-  const hospitals = await Hospital.find()
-  res.status(200).json({ data: hospitals })
+  const { page = 1, limit = 10 } = req.query as any
+
+  try {
+    const hospitals = await Hospital.find()
+      .limit(limit)
+      .skip((page - 1) * limit)
+
+    const count = await Hospital.countDocuments()
+
+    res.status(200).json({
+      totalItems: limit,
+      data: hospitals,
+      totalPages: Math.ceil(count / limit),
+      currentPage: page,
+    })
+  } catch (err) {
+    console.error(err)
+  }
 })
